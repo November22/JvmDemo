dd19ce02697999dd32dd764cb1227c9a6783372c
sen.huang <iths1122@126.com>
2018-05-21 23:42:36 +0800

    java虚拟机第一次提交

---
+++ src/com/chapter2/TestMemory.java
@@ -0,0 +1,34 @@
+package com.chapter2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author sen.huang
+ *         Date: 2018/5/21
+ */
+public class TestMemory {
+    static class OOMObject{
+
+    }
+    public static void main(String[] args) {
+        testOOM();
+    }
+
+    /**
+     * 测试堆内存溢出
+     * java.lang.OutOfMemoryError: Java heap space
+     * 对这个区域的异常，一般手段是先通过内存映像分析工具对Dump出来的 堆存储快照进行分析，
+     * 重点确认内存中的对象是否是必要的。也就是要分清是 内存泄漏（Memory Leak）还是内存溢出（Memory Overflow）
+     *
+     * 被称为 GC(Garbage Collector) Roots 实际上是垃圾回收器需要回收的对象。
+     * 垃圾回收器回收那些不是 GC Roots 的对象并且不再被GC Roots引用的对象。
+     */
+    private static void testOOM(){
+        List<OOMObject> list = new ArrayList<>();
+        while (true){
+            list.add(new OOMObject());
+        }
+    }
+
+}
